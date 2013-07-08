407d31a02efbf8ff1fbe0fb1352f0bf20a72b559
Mike Nomitch <mikenomitch@gmail.com>
2013-07-08 18:12:20 -0500

    revyou name change

--- application.js
+++ application.js
@@ -81,7 +81,7 @@ function highlightSelf(value){
       shadow($("#TeachingTree"));
       break;
     case 1:
-      shadow($("#Two_cents"));
+      shadow($("#RevYou"));
       break;
     case 2:
       shadow($("#Find_by_shortcut"));
@@ -170,26 +170,21 @@ function redirectTo(html){
 // DATA DATA DATA
 
 var projects = [{name:"TeachingTree",header:"TeachingTree",tools: ["Ruby","RubyonRails","HTML5","CSS3","SCSS","Git","Heroku","Javascript","JQuery","Ajax","JQuery","D3", "postgres"], div: "<a href='http://www.teachingtree.co' target='_top'>TeachingTree</a> is an online platform for people who want quick access to concepts in Computer Science and Math lectures. Any user can add videos and time-stamped tags to help other students. </br></br> The site has over 1500 users and has been featured on <a href='http://www.code.org/learn/scratch' target='_top'>Code.org</a>, <a href='http://tech.co/teachingtree-online-learning-2013-04' target='_top'>Tech Cocktail</a>, and <a href='http://www.freetech4teachers.com/2013/02/teaching-tree-video-explanations-of.html' target='_top'>various</a> <a href='http://www.educatorstechnology.com/2013/05/teaching-platform-for-computer-and-math.html' target='_top'>other</a> <a href='http://www.freetech4teachers.com/2013/02/teaching-tree-video-explanations-of.html' target='_top'>blogs</a>. </br></br> Development on the site began in November 2012 and it is written using Ruby on Rails."},
-                {name:"Two_cents",header:"Two Cents", tools: ["Ruby","RubyonRails","HTML5","CSS3","Git","Heroku","Javascript","Coffeescript","JQuery","Ajax","JQuery", "Backbone", "postgres"], div: "<a href='http://curated.herokuapp.com/' target='_top'>Two Cents</a> began as a weekend project, but has grown over time. The site allow users to post stylized 'micro-reviews' of songs and albums with embedded links to YouTube, Spotify, and SoundCloud. </br></br> It was primarily written over the span of two weeks using Ruby on Rails and Backbone. Feel free to check out the source code on <a href='https://github.com/mikenomitch/curated' target='_top'>GitHub</a>."},
+                {name:"RevYou",header:"RevYou", tools: ["Ruby","RubyonRails","HTML5","CSS3","Git","Heroku","Javascript","Coffeescript","JQuery","Ajax","JQuery", "Backbone", "postgres"], div: "<a href='http://revyou.co/' target='_top'>RevYou</a> began as a weekend project, but has grown over time. The site allow users to post stylized 'micro-reviews' of songs and albums with embedded links to YouTube, Spotify, and SoundCloud. </br></br> It was primarily written over the span of two weeks using Ruby on Rails and Backbone JS. Feel free to check out the source code on <a href='https://github.com/mikenomitch/revyou' target='_top'>GitHub</a>."},
                 {name:"Find_by_shortcut",header:"find_by_shortcut", tools: ["Ruby", "RubyonRails"], div: "Find_by_shortcut is a Ruby Gem I wrote to make the rails console a bit simpler. It modifies Active Record to allow for shorcuts while searching through your database. </br></br> Check out <a href='https://github.com/mikenomitch/find_by_shortcut' target='_top'>the documentation on GitHub</a> or <a href='https://rubygems.org/gems/find_by_shortcut' target='_top'>Ruby Gems</a> for more info."},
                 {name:"Griddy_city",header:"Griddy City", tools: ["HTML5", "CSS3", "Javascript"], div: "Griddy City is a simple game that I created using Javascript to help people get to know the grid system of Chicago better. </br></br> It isn't my proudest achievement (it only took a couple hours to make), but it can be very helpful for new Chicago residents. </br></br> It is on <a href='https://github.com/mikenomitch/griddy' target='_top'>GitHub</a> and will soon be hosted elsewhere."}]
 
-var tools = [{name:"Ruby",projects:["TeachingTree","Two_cents","Find_by_shortcut"],div:"Ruby was used to create the back-end of TeachingTree and Two Cents (both of which use Rails). Additionally I used it to write Find_by_shortcut, which is a Ruby gem that uses light metaprogramming in order to make Active Record queries easier. </br></br> The more I get into Javascipt, the more I find myself appreciating just how good Ruby is."},
-             {name:"RubyonRails",projects:["TeachingTree","Two_cents","Find_by_shortcut"],div:"I have used the Rails framework since starting TeachingTree in November, 2012. I have a good working knowledge understanding of its features and best practices. </br></br> I would love to get some experience modifying the source code a bit (but alas, where is the time?)."},
-             {name:"HTML5",projects:["TeachingTree","Two_cents"],div:"There isn't all that much to say about HTML. I use it, just like everybody else. </br></br> If you want to know how good I am with it, check out my sites and judge for yourself."},
-             {name:"CSS3",projects:["TeachingTree","Two_cents"],div:"CSS has become"},
-             {name:"SCSS",projects:["TeachingTree","Two_cents"],div:"scss"},
-             {name:"Git",projects:["TeachingTree","Two_cents"],div:"git"},
-             {name:"Heroku",projects:["TeachingTree","Two_cents"],div:"herroku"},
-             {name:"Javascript",projects:["TeachingTree","Two_cents","Griddy_city"],div:"js"},
-             {name:"Coffeescript",projects:["Two_cents"],div:"coffee"},
-             {name:"Jquery",projects:["TeachingTree","Two_cents"],div:"jquery"},
+var tools = [{name:"Ruby",projects:["TeachingTree","RevYou","Find_by_shortcut"],div:"Ruby was used to create the back-end of TeachingTree and Two Cents (both of which use Rails). Additionally I used it to write Find_by_shortcut, which is a Ruby gem that uses light metaprogramming in order to make Active Record queries easier. </br></br> The more I get into Javascipt, the more I find myself appreciating just how good Ruby is."},
+             {name:"RubyonRails",projects:["TeachingTree","RevYou","Find_by_shortcut"],div:"I have used the Rails framework since starting TeachingTree in November, 2012. I have a good working knowledge understanding of its features and best practices. </br></br> I would love to get some experience modifying the source code a bit (but alas, where is the time?)."},
+             {name:"HTML5",projects:["TeachingTree","RevYou"],div:"There isn't all that much to say about HTML. I use it, just like everybody else. </br></br> If you want to know how good I am with it, check out my sites and judge for yourself."},
+             {name:"CSS3",projects:["TeachingTree","RevYou"],div:"CSS has become"},
+             {name:"SCSS",projects:["TeachingTree","RevYou"],div:"scss"},
+             {name:"Git",projects:["TeachingTree","RevYou"],div:"git"},
+             {name:"Heroku",projects:["TeachingTree","RevYou"],div:"herroku"},
+             {name:"Javascript",projects:["TeachingTree","RevYou","Griddy_city"],div:"js"},
+             {name:"Coffeescript",projects:["RevYou"],div:"coffee"},
+             {name:"Jquery",projects:["TeachingTree","RevYou"],div:"jquery"},
              {name:"Ajax",projects:["TeachingTree"],div:"ajax"},
-             {name:"postgres",projects:["TeachingTree","Two_cents"],div:"postgres"},
+             {name:"postgres",projects:["TeachingTree","RevYou"],div:"postgres"},
              {name:"D3",projects:["TeachingTree"],div:"D3"},
-             {name:"Backbone",projects:["Two_cents"],div:"Backbone"}]
-
-// var teachingTree = "Teachigntree Teachigntree Teachigntree Teachigntree Teachigntree Teachigntree Teachigntree"
-// var twoCents = "2cents 2cents 2cents 2cents 2cents 2cents 2cents 2cents 2cents 2cents 2cents 2cents 2cents"
-// var fbs = "find by shor find by shor find by shor find by shor find by shor find by shor"
-// var griddyCity = "grid cuit grid cuit grid cuit grid cuit grid cuit grid cuit grid cuit grid cuit"
+             {name:"Backbone",projects:["RevYou"],div:"Backbone"}]
