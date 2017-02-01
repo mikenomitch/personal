51641d70c54c6961283e1b00acf4d6bd2c7867dd
Michael Nomitch <mnomitch@instructure.com>
2017-01-31 20:01:43 -0700

    s3 objects set up properly

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,14 +5,22 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "personal" {
-    bucket = "${var.bucket_name}"
-    acl = "public-read"
+  bucket = "${var.bucket_name}"
+  acl = "public-read"
 }
 
-resource "aws_s3_bucket_object" "personal-site" {
+resource "aws_s3_bucket_object" "index" {
   provider = "aws"
 
-  bucket = "${aws_s3_bucket.personal.id}"
-  key    = "index"
+  bucket = "${aws_s3_bucket.personal.bucket}"
+  key    = "index.html"
   source = "../src/index.html"
+}
+
+resource "aws_s3_bucket_object" "styles" {
+  provider = "aws"
+
+  bucket = "${aws_s3_bucket.personal.bucket}"
+  key    = "application.css"
+  source = "../src/application.css"
 }
