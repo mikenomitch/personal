c4c96ca6bc68a115699c98a146214c3e8360dea5
Michael Nomitch <mnomitch@instructure.com>
2017-02-06 08:11:38 -0600

    more tf work

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,10 +4,22 @@ provider "aws" {
   region     = "${var.region}"
 }
 
-resource "aws_s3_bucket" "personal" {
+resource "aws_s3_bucket" "personal_no_www" {
   bucket = "${var.bucket_name}"
   acl = "public-read"
   hosted_zone_id = "${var.zone_id}"
+  policy = "${file("./no_www_bucket_policy.json")}"
+
+  website {
+    index_document = "index.html"
+  }
+}
+
+resource "aws_s3_bucket" "personal_www" {
+  bucket = "www.${var.bucket_name}"
+  acl = "public-read"
+  hosted_zone_id = "${var.zone_id}"
+  policy = "${file("./www_bucket_policy.json")}"
 
   website {
     index_document = "index.html"
@@ -17,7 +29,8 @@ resource "aws_s3_bucket" "personal" {
 resource "aws_s3_bucket_object" "index" {
   provider = "aws"
 
-  bucket = "${aws_s3_bucket.personal.bucket}"
+  acl = "public-read"
+  bucket = "${aws_s3_bucket.personal_no_www.bucket}"
   key    = "index.html"
   source = "../src/index.html"
 }
@@ -25,12 +38,17 @@ resource "aws_s3_bucket_object" "index" {
 resource "aws_s3_bucket_object" "styles" {
   provider = "aws"
 
-  bucket = "${aws_s3_bucket.personal.bucket}"
+  acl = "public-read"
+  bucket = "${aws_s3_bucket.personal_no_www.bucket}"
   key    = "application.css"
   source = "../src/application.css"
 }
 
-resource "aws_route53_zone" "site_zone" {
+resource "aws_route53_zone" "site_zone_www" {
+  name = "www.${var.bucket_name}.com"
+}
+
+resource "aws_route53_zone" "site_zone_no_www" {
   name = "${var.bucket_name}.com"
 }
 
@@ -41,7 +59,7 @@ resource "aws_route53_record" "site" {
 
   alias {
     name = "${var.bucket_name}.s3-website-us-east-1.amazonaws.com"
-    zone_id = "${aws_s3_bucket.personal.hosted_zone_id}"
+    zone_id = "${aws_s3_bucket.personal_no_www.hosted_zone_id}"
     evaluate_target_health = false
   }
 }
