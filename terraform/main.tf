b04a626f69b3dd3fe5be0153f4123d4f9b37afd4
Michael Nomitch <mnomitch@instructure.com>
2017-02-06 08:39:09 -0600

    getting close

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,6 +32,7 @@ resource "aws_s3_bucket_object" "index" {
   acl = "public-read"
   bucket = "${aws_s3_bucket.personal_no_www.bucket}"
   key    = "index.html"
+  content_type  = "text/html"
   source = "../src/index.html"
 }
 
@@ -41,25 +42,30 @@ resource "aws_s3_bucket_object" "styles" {
   acl = "public-read"
   bucket = "${aws_s3_bucket.personal_no_www.bucket}"
   key    = "application.css"
+  content_type  = "text/css"
   source = "../src/application.css"
 }
 
 resource "aws_route53_zone" "site_zone_www" {
-  name = "www.${var.bucket_name}.com"
+  name = "www.${var.bucket_name}"
 }
 
 resource "aws_route53_zone" "site_zone_no_www" {
-  name = "${var.bucket_name}.com"
+  name = "${var.bucket_name}"
 }
 
-resource "aws_route53_record" "site" {
-  zone_id = "${var.zone_id}"
-  name = "${var.bucket_name}.com"
-  type = "A"
+# NOTE:
+# route53 throwing an error, so must manually
+# set up the records in the AWS console
 
-  alias {
-    name = "${var.bucket_name}.s3-website-us-east-1.amazonaws.com"
-    zone_id = "${aws_s3_bucket.personal_no_www.hosted_zone_id}"
-    evaluate_target_health = false
-  }
-}
+# resource "aws_route53_record" "site" {
+#   zone_id = "${var.zone_id}"
+#   name = "${var.bucket_name}"
+#   type = "A"
+
+#   alias {
+#     name = "${var.bucket_name}.s3-website-us-east-1.amazonaws.com"
+#     zone_id = "${aws_s3_bucket.personal_no_www.hosted_zone_id}"
+#     evaluate_target_health = false
+#   }
+# }
